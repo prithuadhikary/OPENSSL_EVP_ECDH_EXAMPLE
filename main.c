90132ca665e8a29972bcbecf9cb2060695e6d51d
Prithu Adhikary <prithu.adhikary@gmaik.com>
2018-02-11 23:03:19 +0530

    - added a few comments describing whats goin on. - added a few memory freeing calls, not all

--- main.c
+++ main.c
@@ -11,23 +11,34 @@ struct derivedKey {
 
 typedef struct derivedKey derivedKey;
 
+// function prototypes
 EVP_PKEY* generateKey();
 EVP_PKEY* extractPublicKey(EVP_PKEY *privateKey);
 derivedKey* deriveShared(EVP_PKEY *publicKey, EVP_PKEY *privateKey);
 
 int main()
 {
+    // Generate Key pairs for Alice And Bob, Using the NIST named curve primve256v1 for generation
+    printf("Alice's generated KeyPair:");
     EVP_PKEY *alicePrivateKey = generateKey();
+
+    printf("\n\nBob's generated KeyPair:");
     EVP_PKEY *bobPrivateKey = generateKey();
 
-    EVP_PKEY *alicePubKey = extractPublicKey(alicePrivateKey);
+    // Extract the public key from the private key of Alice and Bob,
+    // So that Alice can be given Bob's public key and Bob can be given Alice's.
+    // Using ECDH, Alice and Bob will then compute a shared secret, which will be same
 
+    EVP_PKEY *alicePubKey = extractPublicKey(alicePrivateKey);
     EVP_PKEY *bobPubKey = extractPublicKey(bobPrivateKey);
 
+    // Here we give to Alice, Bob's public key and Alice computes the shared secret using her private key.
     derivedKey* secretAlice = deriveShared(bobPubKey, alicePrivateKey);
 
+    // Here we give to Bob, Alice's public key and Bob computes the shared secret using his private key.
     derivedKey* secretBob = deriveShared(alicePubKey, bobPrivateKey);
 
+    //The following lines of code just print out the shared secret computed by Alice and Bob.
     BIGNUM *secretAliceBN = BN_new();
 
     BIGNUM *secretBobBN = BN_new();
@@ -36,13 +47,24 @@ int main()
 
     BN_bin2bn(secretBob->secret, secretBob->length, secretBobBN);
 
-    printf("\nSecret Alice : ");
+    printf("\n\nSecret computed by Alice :\n");
 
     BN_print_fp(stdout, secretAliceBN);
 
-    printf("\nSecret Bob : ");
+    printf("\nSecret computed by Bob : \n");
 
     BN_print_fp(stdout, secretBobBN);
+
+    if(BN_cmp(secretAliceBN, secretBobBN) == 0){
+        printf("\n\nSecrets computed were equal! Magic of ECDH\n\n");
+    }
+
+    //NOTE! It is not recommended to use the computed shared secret as is, usually it should be passed to some
+    //hash function and then used.
+
+    BN_free(secretAliceBN);
+
+    BN_free(secretBobBN);
 
     return 0;
 }
@@ -57,8 +79,10 @@ void handleDerivationErrors(int x){
     printf("%d", x);
 }
 
+/**
+    Generates a key pair and returns it
+*/
 EVP_PKEY* generateKey(){
-
     EVP_PKEY_CTX *paramGenCtx = NULL, *keyGenCtx = NULL;
     EVP_PKEY *params= NULL, *keyPair= NULL;
 
@@ -100,9 +124,15 @@ EVP_PKEY* generateKey(){
 
     BN_print_fp(stdout, y);
 
+    EVP_PKEY_CTX_free(paramGenCtx);
+    EVP_PKEY_CTX_free(keyGenCtx);
+
     return keyPair;
 }
 
+/**
+    Takes in a private key and extracts the public key from it.
+*/
 EVP_PKEY* extractPublicKey(EVP_PKEY *privateKey){
     EC_KEY *ecKey = EVP_PKEY_get1_EC_KEY(privateKey);
     EC_POINT *ecPoint = EC_KEY_get0_public_key(ecKey);
@@ -115,9 +145,15 @@ EVP_PKEY* extractPublicKey(EVP_PKEY *privateKey){
 
     EVP_PKEY_set1_EC_KEY(publicKey, pubEcKey);
 
+    EC_KEY_free(ecKey);
+    EC_POINT_free(ecPoint);
+
     return publicKey;
 }
 
+/**
+    Takes in the private key and peer public key and spits out the derived shared secret.
+*/
 derivedKey* deriveShared(EVP_PKEY *publicKey, EVP_PKEY *privateKey){
 
     derivedKey *dk = (derivedKey *)malloc(sizeof(derivedKey));
@@ -132,11 +168,11 @@ derivedKey* deriveShared(EVP_PKEY *publicKey, EVP_PKEY *privateKey){
 
 	if(1 != EVP_PKEY_derive(derivationCtx, NULL, &dk->length)) handleDerivationErrors(0);
 
-	printf("\n\nSecret Len: %d", dk->length);
-
 	if(NULL == (dk->secret = OPENSSL_malloc(dk->length))) handleDerivationErrors(1);
 
 	if(1 != (EVP_PKEY_derive(derivationCtx, dk->secret, &dk->length))) handleDerivationErrors(2);
+
+    EVP_PKEY_CTX_free(paramCtx);
 
 	return dk;
 }
